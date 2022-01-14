f1efd4290f5a2d6f53e4c7c2ed69d98075ecdbd6
Shleby <shelby.a.huffy@gmail.com>
2022-01-14 13:46:25 -0600

    Updates

---
+++ precache-manifest.85d4ab6191f3c31779b96ff2be0aad3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4917345709f850443223b1ab031f5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1fdd24d41fe11245da4",
+    "url": "/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "c344e11ed96306088d97",
+    "url": "/static/css/main.58cb806c.chunk.css"
+  },
+  {
+    "revision": "c1fdd24d41fe11245da4",
+    "url": "/static/js/2.fb869984.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.fb869984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c344e11ed96306088d97",
+    "url": "/static/js/main.44b49c0c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
